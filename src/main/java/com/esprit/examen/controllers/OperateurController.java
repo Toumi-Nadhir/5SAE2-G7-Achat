1a92cd69d0999ea011c0ea27d5ec449d8098f7a4
khouloud <benmbarek.khouloud@esprit.tn>
2023-10-15 21:53:03 +0100

    khouloud first commit

--- src/main/java/com/esprit/examen/controllers/OperateurController.java
+++ src/main/java/com/esprit/examen/controllers/OperateurController.java
@@ -14,7 +14,7 @@ import io.swagger.annotations.Api;
 @RequestMapping("/operateur")
 @CrossOrigin("*")
 public class OperateurController {
-
+//khouloud
 	@Autowired
 	IOperateurService operateurService;
 	
