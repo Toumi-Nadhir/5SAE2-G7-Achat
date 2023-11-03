04f93dba7230ec68f84b0b2f8bf8f005a43ac7e2
Raedbejaoui <raed.bejaoui@esprit.tn>
2023-11-03 11:04:46 +0100

    raed pom.2

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -3,10 +3,10 @@ package com.esprit.examen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-@EnableScheduling
+
+
 @SpringBootApplication
 public class TpAchatProjectApplication {
 
