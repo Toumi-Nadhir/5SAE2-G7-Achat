fe73124219dc9991bef0b97b9c4979439a5d5dd0
Raedbejaoui <raed.bejaoui@esprit.tn>
2023-11-06 02:27:32 +0100

    raed all tests

--- src/test/java/com/esprit/examen/services/ProduitServiceImpTest.java
+++ src/test/java/com/esprit/examen/services/ProduitServiceImpTest.java
@@ -1,82 +1,86 @@
+
 package com.esprit.examen.services;
 
-import org.junit.Test;
-
-import org.junit.runner.RunWith;
+import com.esprit.examen.entities.Produit;
+import com.esprit.examen.repositories.ProduitRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.junit.Assert.*;
 
-import com.esprit.examen.entities.Produit;
-
-import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+
+
 
 
 @SpringBootTest
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class ProduitServiceImpTest {
 
-    @Mock
-    private IProduitService produitService;
+    private ProduitRepository produitRepository =  Mockito.mock(ProduitRepository.class);;
 
     @InjectMocks
-    private ProduitServiceImpl produitServiceImpl;
+    private ProduitServiceImpl produitService ;
+    @Autowired
+    private ProduitRepository junitProRepo;
+    @Autowired
+    private IProduitService junIProduitService;
 
-    @Test
-    public void testAddProduitWithJUnit() {
-        // Create a sample Produit
-        Produit produit = new Produit();
-        produit.setCodeProduit("sampleCode");
-        produit.setLibelleProduit("Sample Libelle");
-        produit.setPrix(100.0F); // Set the price
+        List<Produit> produitList = new ArrayList() {
+            {
+                add(
+                        Produit.builder()
+                                .idProduit(555L)
+                                .libelleProduit("Raed")
+                                .prix(555L)
 
-        // Call the service method to add the Produit
-        Produit addedProduit = produitServiceImpl.addProduit(produit);
+                                .build()
 
-        // Check if the returned object is not null
-        assertNotNull(addedProduit);
+                );
+            }};
 
-        // Check if the ID is not null
-        assertNotNull(addedProduit.getIdProduit());
+        @Test
+        void retrieveAllProduits() {
 
-        // Additional assertions as needed
+            Mockito.when(produitRepository.findAll()).thenReturn(produitList);
+            List<Produit> produitList = produitService.retrieveAllProduits();
+            assertFalse(produitList.isEmpty());
+            verify(produitRepository).findAll();
+        }
+
+        @Test
+        void addProduit() {
+            Produit newProduit = Produit.builder()
+                    .idProduit(555L)
+                    .libelleProduit("Raed")
+                    .prix(555L)
+                    .build();
+            Produit responseProduit = this.produitService.addProduit(newProduit);
+            assertNotNull(responseProduit);
+            assertEquals(newProduit.getIdProduit(),responseProduit.getIdProduit());
+            this.produitService.deleteProduit(responseProduit.getIdProduit());
+
+        }
+
+        @Test
+        void deleteProduit() {
+        }
+
+        @Test
+        void updateProduit() {
+        }
+
+        @Test
+        void retrieveProduit() {
+        }
     }
-
-    @Test
-    public void testDeleteProduitWithMockito() {
-        // Create a sample Produit
-        Produit produit = new Produit();
-        produit.setIdProduit(1L); // Provide the ID
-        produit.setCodeProduit("sampleCode");
-        produit.setLibelleProduit("Sample Libelle");
-        produit.setPrix(100.0F); // Set the price
-
-        // Mock the behavior for retrieving the Produit
-        Mockito.when(produitService.retrieveProduit(1L)).thenReturn(produit);
-
-        // Delete the Produit
-        produitServiceImpl.deleteProduit(produit.getIdProduit());
-
-        // Verify if the method is called with the correct ID
-        Mockito.verify(produitService).deleteProduit(1L);
-    }
-
-    @Test
-    public void testRetrieveAllProduitsWithMockito() {
-        // Mock the behavior for retrieving all Produits
-        Mockito.when(produitService.retrieveAllProduits()).thenReturn(Arrays.asList(new Produit(), new Produit()));
-
-        // Retrieve all Produits
-        List<Produit> produits = produitServiceImpl.retrieveAllProduits();
-
-        // Check if the list is not empty and contains the expected number of items
-        assertNotNull(produits);
-        //assertEquals(2, produits.size());
-    }
-}
